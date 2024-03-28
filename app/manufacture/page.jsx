00c43c1e1731042d8a0c7c69d0ccf16c902be518
Apachai979 <apachai979@gmail.com>
2024-03-28 12:20:21 +0700

    add conveyer

--- app/manufacture/page.jsx
+++ app/manufacture/page.jsx
@@ -1,1 +1,225 @@
-export default function Manufacture() { }
+import Image from 'next/image';
+
+const lean = [
+    { title: "Производим ровно столько продукции, сколько нужно потребителям", src: "/Manufacture/box.svg" },
+    { title: "Гарантируем заказчику быструю и безопасную доставку продукции", src: "/Manufacture/car.svg" },
+    { title: "При помощи современных технологий мы научились использовать материальные, временные и трудовые ресурсы максимально эффективно", src: "/Manufacture/technik.svg" },
+]
+const lean2 = [
+    { title: "Стремимся к идеальному качеству продукции при нулевом проценте брака", src: "/Manufacture/serct.svg" },
+    { title: "Мы сократили площадь складских помещений до необходимого минимума", src: "/Manufacture/home.svg" },
+    { title: "Каждый сотрудник предприятия имеет возможность раскрыть свой творческий потенциал, так как вовлечен в процесс создания продукта", src: "/Manufacture/persons.svg" },
+]
+
+export default function Manufacture() {
+    return (
+        <>
+            <div className='flex justify-center h-[500px] w-full relative bg-fixed ' >
+                <div className='absolute z-20 w-full h-full bg-txtGreen/50'></div>
+                <Image
+                    src="/manufacture/manuf1.jpeg"
+                    alt="Neoset"
+                    width={4000}
+                    height={2000}
+                    className='object-cover z-10'>
+                </Image >
+
+                <h1 className='z-30 absolute bottom-[300px] text-center text-white font-semibold text-6xl'>Производство OneStep</h1>
+                <h2 className='z-30 absolute bottom-[200px] text-center text-white font-semibold text-3xl'>Современные технологии и мировые стандарты качества</h2>
+            </div>
+
+            <div className='container mx-auto px-4 max-w-[1200px] my-10'>
+                <div className='flex xl:space-x-20 flex-col xl:flex-row space-y-6 xl:space-y-0'>
+                    <div className='flex-1 justify-self-end pt-2'>
+                        <h1 className='text-6xl font-semibold text-mainGreen text-left xl:text-right'>OneStep <span className='text-txtGreen'>— это:</span></h1>
+                    </div>
+                    <div className='flex-1 justify-self-start'>
+                        <ul className='list-disc list-inside text-2xl text-txtGreen leading-relaxed'>
+                            <li>высокотехнологичное оборудование, разработанное с учетом наших требований;</li>
+                            <li>сырье от лучших мировых поставщиков;</li>
+                            <li>внедрение автоматизации всех процессов на базе системы управления предприятием ERP, позволяющей осуществлять контроль на всех этапах производства;</li>
+                            <li>профессионализм, ответственность и увлеченность наших сотрудников</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            {/* nexblock */}
+
+            <div className='container mx-auto px-4 max-w-[1200px]'>
+                <div className='flex justify-center items-center font-xl text-5xl'>
+                    <h1 className='text-center'>
+                        Мы придерживаемся принципов <p className='text-primary_green text-center'>"Бережливого производства"</p>
+                    </h1>
+                </div>
+                <div className='flex flex-col lg:flex-row justify-center items-center my-10'>
+                    <div className='flex flex-col lg:w-2/8 lg:space-y-6 text-lg lg:text-right'>
+                        {lean.map((elem) => {
+                            return (
+                                <div className='flex flex-row-reverse lg:flex-row justify-end items-center  space-x-2 space-x-reverse lg:space-x lg:space-x-4'>
+                                    <p key={elem.title} className=''>{elem.title}</p>
+                                    <Image src={elem.src}
+                                        width={80}
+                                        height={80}
+                                        alt='box'
+                                        className='' />
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className='lg:min-w-[520px] mx-5 my-5 lg:my-0'>
+                        <Image
+                            src="/manufacture/conveer.png"
+                            alt='Конвейер Onestep'
+                            width={540}
+                            height={750}
+                            className='' />
+                    </div>
+                    <div className='flex flex-col lg:w-2/8 lg:space-y-6 text-lg text-left'>
+                        {lean2.map((elem) => {
+                            return (
+                                <div className='flex justify-start items-center  space-x-2 space-x lg:space-x-4'>
+                                    <Image src={elem.src}
+                                        width={80}
+                                        height={80}
+                                        alt='box' />
+                                    <p key={elem.title}>{elem.title}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+
+            {/* nexblock */}
+
+            <div className='container mx-auto px-4 max-w-[1200px]'>
+                <div className='flex justify-center flex-col lg:flex-row'>
+                    <div className='flex-1 flex flex-col lg:items-center'>
+                        <h1 className=' text-txtGreen text-5xl font-semibold whitespace-nowrap w-[420px] pt-3'>О стерилизации</h1>
+                        <p className='text-gray-600 font-semibold lg:text-center text-sm lg:w-[420px] mt-5'>Производится на базе Сибирского центра электронно-лучевой обработки, наукоград Кольцово.</p>
+                        <br />
+                    </div>
+                    <div className='flex-1'>
+                        <p className='text-2xl text-txtGreen'>Наша продукция проходит процедуру стерилизации методом электронно-лучевой обработки.
+                            Данную технологию иначе еще называют холодной пастеризацией (так как эффект достигается без повышения температуры).
+                        </p> <br />
+                        <p className='text-2xl text-txtGreen'>На данный момент это самый безопасный и экологически чистый метод, обеспечивающий не только полную стерильность готовой продукции, инструмента, рабочего материала, сырья, но и существенно повышающий сроки годности.
+                        </p><br />
+                        <ul className='list-disc list-inside text-2xl text-txtGreen'>
+                            <li className='list-none'>Преимущества метода:</li>
+                            <li>Изделия при облучении не нагреваются и не намокают;</li>
+                            <li>Изделия можно использовать сразу после облучения;</li>
+                            <li>Относительно низкая стоимость радиационной обработки, что отражается на себестоимости готовой продукции;</li>
+                            <li>Стерилизованное сырье может сохраняться в несколько раз дольше, чем при стерилизации термической – с помощью высоких температур и химической – с помощью различных реагентов.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className='bg-gray-100 my-10'>
+                <div className='container mx-auto px-4 py-10'>
+                    <div className='flex justify-center flex-col lg:flex-row space-y-10 lg:space-y-0 lg:space-x-10'>
+                        <div className='flex-1 flex flex-col'>
+                            <h1 className='text-5xl text-txtGreen'>Сырье и материалы</h1>
+                            <br />
+                            <hr className='h-1.5 w-24 bg-txtGreen' />
+                            <br />
+                            <p className='text-2xl text-txtGreen'>Качество готовой продукции, в первую очередь, зависит от качества сырья, из которого ее производят.</p>
+                            <br />
+                            <p className='text-2xl text-txtGreen'>Именно поэтому, мы уделяем особое внимание выбору поставщиков и производителей материалов и медицинских изделий для нашей продукции.</p>
+                            <br />
+                            <p className='text-2xl text-txtGreen'>Среди них - ведущие мировые компании, лидеры по производству медицинского текстиля, одноразовых компонентов для хирургических наборов и т.д.</p>
+                            <br />
+                            <p className='text-2xl text-txtGreen'>Наши перевязочные материалы выполнены из традиционной марли высокого качества (100 % хлопок), обладающей прекрасными сорбционными свойствами, воздухопроницаемы и безопасны для пациента, с плотностью 17 и 20 нитей на квадратный сантиметр.</p>
+                            <br />
+                            <p className='text-2xl text-txtGreen'>Все материалы и компоненты, использующиеся в нашем производстве, имеют сертификат качества EC Certificate (о соответствии системы менеджмента качества и технической документации на продукцию европейским стандартам для медицинских изделий).</p>
+                        </div>
+                        <div className='flex-1'>
+                            <Image
+                                src="/manufacture/manuf2.jpg"
+                                alt="Neoset"
+                                width={1000}
+                                height={2000}
+                                className='object-cover h-96 lg:h-auto'>
+                            </Image >
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* nexblock */}
+            <div className='bg-white my-10'>
+                <div className='container mx-auto px-4 py-10'>
+                    <div className='flex justify-center flex-col lg:flex-row space-y-10 lg:space-y-0 '>
+                        <div className='flex-1 flex flex-col bg-gray-200 py-6 px-16 lg:order-2'>
+                            <h1 className='text-5xl text-txtGreen'>Склад и логистика</h1>
+                            <br />
+                            <hr className='h-1.5 w-24 bg-txtGreen' />
+                            <br />
+                            <p className='text-2xl text-txtGreen'>Складские мощности позволяют обеспечить производство сырьем на несколько месяцев бесперебойной работы.</p>
+                            <br />
+                            <p className='text-2xl text-txtGreen'>Также, возможности складского хранения позволяют иметь в наличии достаточное количество готовой продукции, необходимой для наших заказчиков.</p>
+                            <br />
+                            <p className='text-2xl text-txtGreen'>Благодаря внедренной на производстве системе учета и контроля ERP, имеется возможность планировать складское хранение, а также регулировать сроки и стабильность поставки товара заказчику.</p>
+                        </div>
+                        <div className='flex-1 lg:px-16 lg:order-1'>
+                            <Image
+                                src="/manufacture/sklad1.jpg"
+                                alt="Neoset"
+                                width={1000}
+                                height={2000}
+                                className='object-cover h-64 lg:h-auto'>
+                            </Image >
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            {/* nexblock */}
+
+
+            <div className='bg-gray-100 my-10'>
+                <div className='container mx-auto px-4 py-10'>
+                    <div className='flex justify-center flex-col lg:flex-row space-y-10 lg:space-y-0 lg:space-x-10'>
+                        <div className='flex-1 flex flex-col'>
+                            <h1 className='text-5xl text-txtGreen'>Упаковка: стерильность и безопасность.</h1>
+                            <br />
+                            <hr className='h-1.5 w-24 bg-txtGreen' />
+                            <br />
+                            <p className='text-2xl text-txtGreen'>Упаковка наборов NeoSet выполнена из твердого материала, обеспечивающего безопасность хранения и транспортировки.</p>
+                            <div className='text-2xl text-txtGreen'> Выполняет следующие функции:
+                                <ul className='list-inside list-disc pl-4 leading-normal'>
+                                    <li>систематизирующую - распределению составных элементов комплекта;</li>
+                                    <li>сортировочную - позволяет отделять чистый материал от использованного;</li>
+                                    <li>функцию одноразовых емкостей - может вмещать до двух разных видов жидкостей.</li>
+                                </ul>
+                            </div>
+                            <br />
+                            <div className='text-2xl text-txtGreen'>Безопасность и простота в использовании. Cкругленные углы позволяют избежать:
+                                <ul className='list-inside list-disc pl-4 leading-normal'>
+                                    <li>риска пораниться во время вскрытия;</li>
+                                    <li>риска повреждения соседней упаковки во время транспортировки и хранения.</li>
+                                </ul>
+                            </div>
+                            <br />
+                            <p className='text-2xl text-txtGreen'>Сохранение стерильности упаковки гарантировано в течение всего срока хранения.</p>
+
+                        </div>
+                        <div className='flex-1'>
+                            <Image
+                                src="/manufacture/nabor1.png"
+                                alt="Neoset"
+                                width={1000}
+                                height={2000}
+                                className='object-cover h-96 lg:h-auto'>
+                            </Image >
+                        </div>
+                    </div>
+                </div>
+            </div >
+        </>
+    )
+}
