f915cdfbd9e66540e47b22b466a622966f944621
Apachai979 <apachai979@gmail.com>
2024-04-04 12:14:42 +0700

    nice header

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -18,7 +18,7 @@ export default function Navigation() {
     const [state, setState] = useState(true)
 
     function handleMenu() {
-        if (state) ulRef.current.className += ' top-[80px] opacity-100 '
+        if (state) ulRef.current.className += ' top-[64px] opacity-100 '
         else ulRef.current.className = ' sm920:flex sm920:flex-1 sm920:items-center sm920:justify-evenly z-[-1] sm920:z-auto sm920:static absolute bg-body_bg sm920:max-w-[880px] w-full left-0 sm920:w-auto sm920:py-0 py-4 sm920:pl-0 pl-7 text-sm lg:text-base sm920:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 '
 
         setState((prev) => !prev)
@@ -26,7 +26,7 @@ export default function Navigation() {
     }
 
     useEffect(() => {
-        const intervalId = setInterval(handleMenu, 3000)
+        const intervalId = setInterval(handleMenu, 1000)
         return () => { clearInterval(intervalId) }
     }, [])
 
@@ -34,22 +34,22 @@ export default function Navigation() {
 
     return (
         <>
-            <ul ref={ulRef} className="sm920:flex sm920:flex-1 sm920:items-center sm920:justify-evenly z-[-1] sm920:z-auto sm920:static absolute bg-body_bg sm920:max-w-[880px] w-full left-0 sm920:w-auto xl:px-8 sm920:px-4 sm920:py-0 py-4 pl-7 text-base sm920:text-sm lg:text-base sm920:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 ">
+            <ul ref={ulRef} className="rounded-b-3xl sm920:rounded-none sm920:flex sm920:flex-1 sm920:items-center sm920:justify-evenly z-[-1] sm920:z-auto sm920:static absolute bg-gray-200 sm920:bg-body_bg sm920:max-w-[880px] w-full left-0 sm920:w-auto xl:px-8 sm920:px-4 sm920:py-0 py-4 pl-7 text-base sm920:text-sm lg:text-base sm920:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 ">
 
-                <li className="mx-4 my-6 sm920:my-0 sm920:mx-0 ">
-                    <Link href="/catalogs" className={pathname === "/catalogs" ? "shadow-inner bg-contrast_green shadow-gray-600/50 text-white rounded-full px-6 py-2 " : "bg-primary_green text-white rounded-full shadow-md shadow-night_green/50 hover:shadow-inner hover:bg-contrast_green hover:shadow-gray-600/50 px-6 py-2 text-center transition-all hover:text-white "}>Каталог</Link>
+                <li className="mx-4 mb-8 mt-4 sm920:my-0 sm920:mx-0 text-center ">
+                    <Link href="/catalogs" className={pathname === "/catalogs" ? "shadow-inner bg-contrast_green shadow-gray-600/50 text-white rounded-full px-14 py-2 sm920:px-6 sm920:py-2 " : "bg-primary_green text-white rounded-full shadow-md shadow-night_green/50 hover:shadow-inner hover:bg-contrast_green hover:shadow-gray-600/50 px-14 py-2 sm920:px-6 sm920:py-2 text-center transition-all hover:text-white "}>Каталог</Link>
                 </li>
 
                 {navItems.map((link) => {
                     const isActive = pathname === link.href;
                     return (
-                        <li key={link.label} className="mx-4 my-6 sm920:my-0 sm920:mx-0 ">
+                        <li key={link.label} className="mx-4 my-6 sm920:my-0 sm920:mx-0 text-center">
                             <Link href={link.href} className={isActive ? "px-1 text-primary_green duration-300"
                                 : "px-1 hover:text-primary_green duration-300"}>{link.label}</Link>
                         </li>
                     )
                 })}
-                <div className="-space-y-1.5 flex-none block sm920:hidden ">
+                <div className="-space-y-1.5 flex-none block sm920:hidden text-center">
                     <p className="text-xs font-semibold text-stone-400">Тел./WhatsApp</p>
                     <p className="lg:text-lg text-base font-semibold">+7 (495) 927-48-47</p>
                 </div>
